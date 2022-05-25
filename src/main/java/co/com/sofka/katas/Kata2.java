c4d5c4aad9295016623b032e9ef663ef94bf694f
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-25 12:21:21 -0500

    [EDIT]Kata 2. Se completa exitosamente la Kata 2.

--- src/main/java/co/com/sofka/katas/Kata2.java
+++ src/main/java/co/com/sofka/katas/Kata2.java
@@ -1,10 +1,10 @@
 package co.com.sofka.katas;
 
-import com.google.common.collect.ImmutableList;
 import co.com.sofka.model.Movie;
 import co.com.sofka.util.DataUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -12,9 +12,16 @@ import java.util.List;
     Output: List of Integers
 */
 public class Kata2 {
+
+    private Kata2(){
+        throw new IllegalStateException("Kata 2");
+    }
+
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(1, 2, 3);
+        return movies.stream().filter(p -> p.getRating().equals(5.0))
+                                            .map(Movie::getId)
+                                            .collect(Collectors.toList());
     }
 }

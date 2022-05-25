901e0420bcbb78c4965502dcbd9b6156420319f2
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-25 12:30:19 -0500

    [EDIT]Kata 3. Se completa exitosamente la Kata 3.

--- src/main/java/co/com/sofka/katas/Kata3.java
+++ src/main/java/co/com/sofka/katas/Kata3.java
@@ -1,10 +1,12 @@
 package co.com.sofka.katas;
 
-import com.google.common.collect.ImmutableList;
+import co.com.sofka.model.Movie;
 import co.com.sofka.model.MovieList;
 import co.com.sofka.util.DataUtil;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -12,9 +14,17 @@ import java.util.List;
     Output: List of Integers
 */
 public class Kata3 {
+
+    private Kata3(){
+        throw new IllegalStateException("Kata 3");
+    }
+
     public static List<Integer> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(1, 2, 3);
+        return movieLists.stream().map(MovieList::getVideos)
+                                    .flatMap(Collection::stream)
+                                    .map(Movie::getId)
+                                    .collect(Collectors.toList());
     }
 }

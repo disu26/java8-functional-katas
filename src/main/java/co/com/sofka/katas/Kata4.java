82f0a44ae8bb958ea7725e6129a74f7a04bcec09
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-25 12:49:29 -0500

    [EDIT]Kata 4. Se completa exitosamente la Kata 4.

--- src/main/java/co/com/sofka/katas/Kata4.java
+++ src/main/java/co/com/sofka/katas/Kata4.java
@@ -1,13 +1,13 @@
 package co.com.sofka.katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import co.com.sofka.model.BoxArt;
 import co.com.sofka.model.MovieList;
 import co.com.sofka.util.DataUtil;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -15,9 +15,19 @@ import java.util.Map;
     Output: List of ImmutableMap.of("id", "5", "title", "Bad Boys", "boxart": BoxArt)
 */
 public class Kata4 {
+
+    private Kata4(){
+        throw new IllegalStateException("Kata 4");
+    }
+
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        return movieLists.stream().map(MovieList::getVideos)
+                                    .flatMap(Collection::stream)
+                                    .map(p -> Map.of("id", p.getId(), "title", p.getTitle(), "boxart",
+                                            new BoxArt(150, 200,
+                                                    p.getBoxarts().stream().map(BoxArt::getUrl).toString())))
+                                    .collect(Collectors.toList());
     }
 }

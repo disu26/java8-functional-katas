9ba89ab4ca3e150fd7c5ac50b30dfd980f49739d
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-25 22:55:01 -0500

    [EDIT]Kata 8. Kata 8 completada exitosamente.

--- src/main/java/co/com/sofka/katas/Kata8.java
+++ src/main/java/co/com/sofka/katas/Kata8.java
@@ -1,13 +1,14 @@
 package co.com.sofka.katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
+import com.codepoetics.protonpack.StreamUtils;
 import co.com.sofka.model.Bookmark;
 import co.com.sofka.model.Movie;
 import co.com.sofka.util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
     Goal: Combine videos and bookmarks by index (StreamUtils.zip) (https://github.com/poetix/protonpack)
@@ -15,12 +16,19 @@ import java.util.Map;
     Output: List of ImmutableMap.of("videoId", "5", "bookmarkId", "3")
 */
 public class Kata8 {
+
+    private Kata8(){
+        throw new IllegalStateException("Kata 8");
+    }
+
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
         List<Bookmark> bookMarks = DataUtil.getBookMarks();
 
-        // StreamUtils.zip()
+        Stream<Movie> streamMovies = movies.stream();
+        Stream<Bookmark> streamBookmarks = bookMarks.stream();
 
-        return ImmutableList.of(ImmutableMap.of("videoId", 5, "bookmarkId", 3));
+        return StreamUtils.zip(streamMovies, streamBookmarks, (m, b) -> Map.of("videoId", m.getId(), "bookmarkId", b.getId()))
+                            .collect(Collectors.toUnmodifiableList());
     }
 }

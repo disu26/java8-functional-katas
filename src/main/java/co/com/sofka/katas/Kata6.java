6c5683aa632becf0f234569fa74c464b756033df
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-25 20:43:30 -0500

    [EDIT]Kata 6. Kata 6 completada exitosamente.

--- src/main/java/co/com/sofka/katas/Kata6.java
+++ src/main/java/co/com/sofka/katas/Kata6.java
@@ -1,9 +1,12 @@
 package co.com.sofka.katas;
 
+import co.com.sofka.model.BoxArt;
 import co.com.sofka.model.Movie;
 import co.com.sofka.util.DataUtil;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -11,9 +14,29 @@ import java.util.List;
     Output: String
 */
 public class Kata6 {
+    private Kata6(){
+        throw new IllegalStateException("Kata 6");
+    }
+
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return "someUrl";
+        Optional<BoxArt> largestBoxArt = movies.stream()
+                                            .map(Movie::getBoxarts)
+                                            .flatMap(Collection::stream)
+                                            .reduce((largest, boxArt) -> {
+                                                Integer largestLarge = largest.getHeight() * largest.getWidth();
+                                                Integer boxArtLarge = boxArt.getHeight() * boxArt.getWidth();
+
+                                                if(Boolean.TRUE.equals(lowerThan(largestLarge, boxArtLarge))){
+                                                    largest = boxArt;
+                                                }
+                                                return largest;
+                                            });
+        return largestBoxArt.map(BoxArt::getUrl).orElse(null);
+    }
+
+    public static Boolean lowerThan(Integer num1, Integer num2){
+        return num1 < num2;
     }
 }

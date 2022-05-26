64a8c61cbf0aceac46f4e025bd61710428d5f4d5
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-25 19:44:48 -0500

    [EDIT]Kata 5. Kata 5 completada exitosamente.

--- src/main/java/co/com/sofka/katas/Kata5.java
+++ src/main/java/co/com/sofka/katas/Kata5.java
@@ -4,6 +4,7 @@ import co.com.sofka.model.Movie;
 import co.com.sofka.util.DataUtil;
 
 import java.util.List;
+import java.util.Optional;
 
 /*
     Goal: Retrieve the largest rating using reduce()
@@ -11,9 +12,23 @@ import java.util.List;
     Output: Double
 */
 public class Kata5 {
+
+    private Kata5(){
+        throw new IllegalStateException("Kata 5");
+    }
+
     public static Double execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return 3.0;
+        Optional<Movie> maxRating = movies.stream()
+                .reduce((max, rating) -> {
+                    if(max.getRating() < rating.getRating()){
+                        max = rating;
+                    }
+                    return max;
+                });
+
+        return maxRating.map(Movie::getRating).orElse(null);
+
     }
 }

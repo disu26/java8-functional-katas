4fe42e2ee449c61c4f8d888b637d4a90377de89f
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-25 22:21:20 -0500

    [EDIT]Kata 7. Kata 7 sin completar.

--- src/main/java/co/com/sofka/katas/Kata7.java
+++ src/main/java/co/com/sofka/katas/Kata7.java
@@ -1,12 +1,15 @@
 package co.com.sofka.katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
+import co.com.sofka.model.BoxArt;
+import co.com.sofka.model.Movie;
 import co.com.sofka.model.MovieList;
 import co.com.sofka.util.DataUtil;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the id, title, and smallest box art url for every video
@@ -14,9 +17,38 @@ import java.util.Map;
     Output: List of ImmutableMap.of("id", "5", "title", "Bad Boys", "boxart": "url)
 */
 public class Kata7 {
+
+    private Kata7(){
+        throw new IllegalStateException("Kata 7");
+    }
+
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
+        Optional<BoxArt> smallestBoxArt = movieLists.stream()
+                .map(MovieList::getVideos)
+                .flatMap(Collection::stream)
+                .map(Movie::getBoxarts)
+                .flatMap(Collection::stream)
+                .reduce((smallest, boxArt) -> {
+                    Integer smallestLarge = smallest.getHeight() * smallest.getWidth();
+                    Integer boxArtLarge = boxArt.getHeight() * boxArt.getWidth();
+
+                    if(Boolean.TRUE.equals(greaterThan(smallestLarge, boxArtLarge))){
+                        smallest = boxArt;
+                    }
+                    return smallest;
+                });
+
+        return movieLists.stream()
+                            .map(MovieList::getVideos)
+                            .flatMap(Collection::stream)
+                            .filter(m -> m.getBoxarts().equals(smallestBoxArt.get()))
+                            .map(m -> Map.of("id", m.getId(), "title", m.getTitle(), "boxart", smallestBoxArt.get().getUrl()))
+                            .collect(Collectors.toUnmodifiableList());
+    }
+
+    public static Boolean greaterThan(Integer num1, Integer num2){
+        return num1 < num2;
     }
 }
